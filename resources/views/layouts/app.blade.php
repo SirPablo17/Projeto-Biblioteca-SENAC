2c6cdcc88143ffcb6b29b4df0349e8a0a3d7881e
SirPablo17 <pablo.rosario2019@gmail.com>
2025-11-28 17:31:31 +0000

    Ajustando o Vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Livraria Online</title>
-    <link rel="icon" type="image/x-icon" href="\img\logo.svg">
+    
+    {{-- CORREÇÃO 1: Use barra normal (/) para funcionar no Linux --}}
+    <link rel="icon" type="image/x-icon" href="/img/logo.svg">
 
     {{-- Font Awesome --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"/>
@@ -13,22 +15,23 @@
     {{-- Swiper CSS --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
 
+    {{-- CORREÇÃO 2: Removido o lixo de código no final --}}
     @vite([
-        'resources/css/app.css',       // <--- OBRIGATÓRIO (Tailwind)
-        'resources/js/app.js',         // <--- OBRIGATÓRIO (Scripts padrão)
-        'resources/css/style.css',     // <--- Seus estilos customizados
+        'resources/css/app.css',       // OBRIGATÓRIO (Tailwind)
+        'resources/js/app.js',         // OBRIGATÓRIO (Scripts padrão)
+        'resources/css/style.css',     // Seus estilos customizados
         'resources/js/header-wrapper.js',
         'resources/js/header.js',
         'resources/js/hero-carousel.js'
-    ]) 'resources/js/header.js', 'resources/js/hero-carousel.js'])
+    ])
 </head>
 <body>
-    {{-- HEADER (Vamos incluir como um componente depois) --}}
+    {{-- HEADER --}}
     <div class="header-sticky-wrapper">
         @include('partials.header')
     </div>
 
-    {{-- CONTEÚDO DINÂMICO (Aqui entra o miolo de cada página) --}}
+    {{-- CONTEÚDO DINÂMICO --}}
     <main>
         @yield('content')
     </main>
